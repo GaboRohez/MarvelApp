74709a7120daa306883647b4302780f3e15ae734
Gabo Rohez <gabow95k@gmail.com>
2022-09-22 14:55:17 -0500

    create splash screen

---
+++ app/src/main/java/com/example/marvelapp/ui/SplashScreen.kt
@@ -0,0 +1,14 @@
+package com.example.marvelapp.ui
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.marvelapp.MainActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+        finish()
+    }
+}
